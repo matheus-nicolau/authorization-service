f4101fad7a7cdf3c2cfc8bc6ada3a2a4269d2cb7
matheus-nicolau <matheusnicolau.dev@gmail.com>
2025-03-15 08:30:07 -0400

    feat: implements unitary tests.

--- src/test/java/com/example/authorization_service/service/UserServiceTest.java
+++ src/test/java/com/example/authorization_service/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.authorization_service.service;
 
 import com.example.authorization_service.dto.UserCreateDTO;
+import com.example.authorization_service.dto.UserReturnDTO;
 import com.example.authorization_service.entity.UserEntity;
 import com.example.authorization_service.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,8 +16,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -54,32 +56,48 @@ class UserServiceTest {
         // GIVEN
         when(usersRepository.findByLogin(anyString())).thenReturn(Optional.of(userEntity));
         // WHEN
-        UserEntity userByLogin = userService.findByLogin(anyString());
+        var userByLogin = userService.findByLogin(anyString());
         // THEN
         assertEquals(userByLogin, userEntity);
     }
 
     @Test
-    @DisplayName(" Description ")
-    void given_when_then() {
+    @DisplayName("Should be find a user by email.")
+    void givenHaveAEmail_whenSearchUserByEmail_thenReturnThisUser() {
         // GIVEN
-
-
+        when(usersRepository.findByEmail(anyString())).thenReturn(Optional.of(userEntity));
         // WHEN
-
-
+        var userByEmail = userService.findByEmail("nicolau.test@gmail.com");
         // THEN
-
-
+        assertEquals(userByEmail, userEntity);
     }
 
+    @Test
+    @DisplayName("Should be find all users.")
+    void givenHaveForAUserList_whenSearchThisList_thenReturnTheList() {
+        // GIVEN
+        var userReturnDTO = createUserReturnDTO();
+        when(usersRepository.findAll()).thenReturn(List.of(userEntity));
+        // WHEN
+        var allUsers = userService.findAllUsers();
+        // THEN
+        assertEquals(allUsers, List.of(userReturnDTO));
+    }
 
     private UserCreateDTO createUserDTO() {
         return new UserCreateDTO(
                             "matheus",
                             "1234",
                             List.of("ADMIN"),
-                            "nicolau.test@gmail.com");
+                            "nicolau.test@gmail.com"
+        );
     }
 
+    private UserReturnDTO createUserReturnDTO() {
+        return new UserReturnDTO(
+                            "matheus",
+                            List.of("ADMIN"),
+                            "nicolau.test@gmail.com"
+        );
+    }
 }
